e18e095aec6ff968854047ac67ee70dac0295981
weekend79 <mortenviken@yahoo.com>
2020-03-08 18:28:06 +0000

    contact form, retriving and posting data with a response to the user

--- flask/run.py
+++ flask/run.py
@@ -1,9 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.secret_key = 'some_secret'
 
 @app.route('/')
 def index():
@@ -16,8 +16,23 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
-@app.route("/contact")
+@app.route("/about<member_name>")
+def about_member(member_name):
+    member ={}
+
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)  
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!". format(request.form["name"]))
+
     return render_template("contact.html", page_title="Contact")
 
 @app.route("/careers")
